6e4d104293a6bc845f7ccd9520f680d7281d80eb
Sharandeep Ghosh <sharandeep23@yahoo.com>
2020-07-01 11:29:49 +0600

    Some Refactoring

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 // Selecting color inputs
 const colorEls = document.querySelectorAll("input[type=color]");
 
-// Syncing initial Colors with the the document
+// Selecting the paragraph that will display the code for the gradient
+const p = document.querySelector(".rule-set");
+
+// Syncing initial Colors[from HTML's value attribute] with the body of the document
 setGradient();
 
-// Creating and appending a new paragraph element to display information about the background
-const p = document.createElement("p");
-// Adding a class named rule-set
-p.className = "rule-set";
-document.body.append(p);
-
-// Setting default info about the gradient
+// Setting the code for the gradient
 setText();
 
-// Attaching event listeners to color inputs to change color and update info in accordance with the input
+// Attaching event listeners to color inputs to change color and update code in accordance with the input
 for (const colorEl of colorEls) {
     colorEl.addEventListener("input", () => {
         setGradient();
@@ -21,14 +18,10 @@ for (const colorEl of colorEls) {
     });
 }
 
-function getGradient() {
-    return `linear-gradient(to right, ${colorEls[0].value}, ${colorEls[1].value})`;
-}
-
 function setGradient() {
-    document.body.style.backgroundImage = getGradient();
+    document.body.style.backgroundImage = `linear-gradient(to right, ${colorEls[0].value}, ${colorEls[1].value})`;
 }
 
 function setText() {
-    p.textContent = getGradient() + ";";
+    p.textContent = document.body.style.backgroundImage + ";";
 }
